ea3fdf7ebb1c2ec25c4ebe3911b3d7595dff2437
Aseyed <aseyedmostafaho@gmail.com>
2019-02-13 18:02:44 +0330

    make hello world

--- jooya/things/models.py
+++ jooya/things/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Things(models.Model):
+    description = models.CharField(max_length=4000)
+    date_added = models.DateField(auto_now=True)
+    user = models.ForeignKey(User, related_name='things')
+    #image = models.ImageField()
+    
+    
