38428c96bfe175bd0193bed43187f4f279a60437
zargarzadehm <moein.zargarzadeh@gmail.com>
2019-02-14 16:40:46 +0330

    Add model imagefield

--- jooya/things/models.py
+++ jooya/things/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Things(models.Model):
+
     description = models.CharField(max_length=4000)
     date_added = models.DateField(auto_now=True)
     user = models.ForeignKey(User, related_name='things')
-    #image = models.ImageField()
-    
-    
+    image = models.ImageField(upload_to='things_pic', blank=True)
