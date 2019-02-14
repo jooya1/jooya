2fbc72a6298c6ba16c8abbb4735ee10fe6d32b93
zargarzadehm <moein.zargarzadeh@gmail.com>
2019-02-15 01:39:15 +0330

    Add function index request

--- jooya/things/views.py
+++ jooya/things/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Hello World!')
+
+
+def index(request):
+    return render(request, 'things/index.html')
