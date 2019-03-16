bb8ab261f6ca472e5cbd92b22f426d5679367252
zargarzadehm <moein.zargarzadeh@gmail.com>
2019-03-16 14:09:31 +0330

    Update requirements

--- jooya/things/admin.py
+++ jooya/things/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
-from .models import Things
 # Register your models here.
-admin.site.register(Things)
--- jooya/things/models.py
+++ jooya/things/models.py
@@ -1,29 +1,28 @@
-from django.contrib.auth.decorators import login_required
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-from django.forms import modelformset_factory
 
 
 class Things(models.Model):
 
-    title = models.CharField(max_length=1000, default='title')
+    name = models.CharField(max_length=40)
     description = models.CharField(max_length=4000)
     date_added = models.DateField(auto_now=True)
     user = models.ForeignKey(User, related_name='things')
     image = models.ImageField(upload_to='things_pic', blank=True)
-'''
-@login_required
-def AddNewThing(request):
-    ThingsFormSet = modelformset_factory(Things, fields=('name', 'price', 'category'), extra=0)
-    data = request.POST or None
-    formset = ProductFormSet(data=data, queryset=Product.objects.filter(user=request.user))
-    for form in formset:
-        form.fields['category'].queryset = Category.objects.filter(user=request.user)
 
-    if request.method == 'POST' and formset.is_valid():
-        formset.save()
-        return redirect('products_list')
+    def type_to_string(self):
+        """Convert the type field to its string representation
+        (the boneheaded way).
+        """
+        if self.type == 1:
+            return "Sedan"
+        elif self.type == 2:
+            return "Truck"
+        else:
+            return "SUV"
 
-    return render(request, 'products/products_formset.html', {'formset': formset})'''
+    def __str__(self):
+        # Built-in attribute of django.contrib.auth.models.User !
+        return self.name
