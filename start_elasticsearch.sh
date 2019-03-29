3b3b953f9c816101f95d539dea5f32f2d1bab1b7
zargarzadehm <moein.zargarzadeh@gmail.com>
2019-03-30 02:32:47 +0430

    Add docker file for server elasticsearch

---
+++ start_elasticsearch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Start elasticsearch in a docker container
+
+ES_VERSION=${ES_VERSION:-"6.1.3"}
+ES_TEST_SERVER=${ES_TEST_SERVER:-"http://localhost:9200"}
+
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+exec docker run -d \
+    -e path.repo=/tmp \
+    -e "repositories.url.allowed_urls=http://*" \
+    -e node.attr.testattr=test \
+    -e ES_HOST=$ES_TEST_SERVER \
+    -v $SOURCE_DIR/../elasticsearch:/code/elasticsearch \
+    -v /tmp:/tmp \
+    -p "9200:9200" \
+docker.elastic.co/elasticsearch/elasticsearch-oss:6.2.1
