6dcedc6743d11ec2955ddc0d1d42d028c9f54abc
zargarzadehm <moein.zargarzadeh@gmail.com>
2019-03-31 17:27:10 +0430

    remove user from form

--- jooya/things/forms.py
+++ jooya/things/forms.py
@@ -1,10 +1,11 @@
 from django import forms
-from django.contrib.auth.models import User
-
 from  .models import Things
+
 
 class ThingsForm(forms.ModelForm):
 
+    description = forms.CharField(widget=forms.Textarea)
+
     class Meta:
         model = Things
-        fields = ['title', 'description', 'user', 'image']
+        fields = ['title', 'description', 'image']
