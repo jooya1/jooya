63bce1c23f2203fbd57a3b18c50dc86096776ef1
mohammad-abdollahi <m.abdollahi776@gmail.com>
2019-04-09 19:52:48 +0430

    added requiered field to image in addForm

--- jooya/things/forms.py
+++ jooya/things/forms.py
@@ -3,8 +3,11 @@ from  .models import Things
 
 
 class ThingsForm(forms.ModelForm):
-
     description = forms.CharField(widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super(ThingsForm, self).__init__(*args, **kwargs)
+        self.fields['image'].required = True
 
     class Meta:
         model = Things
