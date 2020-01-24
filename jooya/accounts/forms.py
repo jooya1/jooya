5454c9b36e8c27f6b9d032d5e09b3465917843c8
zargarzadehm <moein.zargarzadeh@gmail.com>
2020-01-25 00:34:27 +0330

    Change flow login

--- jooya/accounts/forms.py
+++ jooya/accounts/forms.py
@@ -4,11 +4,18 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
+    email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput(), label='ایمیل')
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+        labels = {
+            "first_name": "نام",
+            "last_name": "نام خانوادگی",
+            "username": "نام کاربری",
+            "password1": "پسورد",
+            "password2": "تکرار پسورد"
+        }
 
 
 class UserInformationUpdateForm(forms.ModelForm):
