392d1cf3d776f3191f3b08d59ef1f6b9c6c00a10
zargarzadehm <moein.zargarzadeh@gmail.com>
2020-01-25 00:38:39 +0330

    Complete test

--- jooya/accounts/tests/test_form_signup.py
+++ jooya/accounts/tests/test_form_signup.py
@@ -7,6 +7,7 @@ from ..forms import SignUpForm
 class SignUpFormTest(TestCase):
     def test_form_has_fields(self):
         form = SignUpForm()
-        expected = ['username', 'email', 'password1', 'password2', ]
+        expected = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
         actual = list(form.fields)
         self.assertSequenceEqual(expected, actual)
