81941b7c015c43c5317b0847b4e2d6a09ff97564
mohammad-abdollahi <m.abdollahi776@gmail.com>
2020-01-25 22:26:46 +0330

    changes in login form

--- jooya/accounts/forms.py
+++ jooya/accounts/forms.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['username'].help_text = 'حداکثر ۱۵۰ کاراکتر - تنها شامل حروف و اعداد انگلیسی باشد.'
+        self.fields['password1'].help_text = '<li>پسورد شما نباید شبیه اطلاعات شخصی شما باشد.</li>' \
+                                             '<li>پسورد شما باید حداقل شامل ۸ کاراکتر باشد.</li>' \
+                                             '<li>پسورد شما نباید از پسورد‌های رایج باشد.</li>' \
+                                             '<li>پسورد شما نباید فقط شامل اعداد باشد.</li>'
+        self.fields['password2'].help_text = 'پسورد خود را دوباره وارد نمایید.'
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput(), label='ایمیل')
 
     class Meta:
